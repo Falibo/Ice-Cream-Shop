814fa3c74aca2801c6f9d24ad010369eb1105809
Raf <ach.elismaili@gmail.com>
2025-03-25 22:44:55 -0400

    Update vite.config.js for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    open: true
-  },
-  base: '/Ice-Cream-Shop/',
-<<<<<<< HEAD
-});
-=======
-});
->>>>>>> origin/main
+  base: '/Ice-Cream-Shop/'
+})
